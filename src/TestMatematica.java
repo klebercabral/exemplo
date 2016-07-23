7111e047f2f8f3735ea83569239fcf61defe1ef1
Rodrigo Pimenta Reis <jenkins@MacBook-Air-de-Rodrigo.local>
2016-07-23 13:42:43 -0300

    TestePassando

--- src/TestMatematica.java
+++ src/TestMatematica.java
@@ -5,6 +5,6 @@ public class TestMatematica {
 
         @Test
         public void testSoma() {
-                assertEquals("10 + 1 = 11", 10, Matematica.soma(10, 1));    
+                assertEquals("10 + 1 = 11", 11, Matematica.soma(10, 1));    
         }
 }
